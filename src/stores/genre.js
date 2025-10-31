65733b0fbe2a22d66d37ac8e375fa5fcf3a2d223
AndreRicardoSc <andrenarutto7@gmail.com>
2025-10-31 11:11:17 -0300

    FEAT: Criando a store para generos dos filmes #1

---
+++ src/stores/genre.js
@@ -0,0 +1,45 @@
+import { defineStore } from "pinia";
+import { computed, reactive } from "vue";
+import { useMovieStore } from "./movie";
+import { watch } from "vue";
+
+export const useGenreStore = defineStore('genreStore', () => {
+
+    const movieStore = useMovieStore();
+
+    const state = reactive({
+        genres: [],
+        currentGenre: 0,
+    })
+
+    const genres = computed(() => state.genres);
+    const currentGenre = computed(() => state.currentGenre);
+
+    const getGenresIds = async() => {
+        const genreIds = new Set();
+        for(let movie of movieStore.movies){
+            for(let id of movie.genre_ids){
+                genreIds.add(id);
+            }
+        }
+        state.genres = [...genreIds];
+    }
+
+    const setCurrentGenre = (id) => {
+        state.currentGenre = id;
+        movieStore.getMovies(1, id);
+    }
+
+    watch(
+        () => movieStore.movies,
+        () => getGenresIds(),
+        { deep: true }
+    );
+
+    return {
+        genres,
+        currentGenre,
+        getGenresIds,
+        setCurrentGenre
+    }
+})
