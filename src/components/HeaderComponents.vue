1c6b162ad2d3c30fe200bfe00511d81b07034c8e
Davidospassos <davidospassos.848@gmail.com>
2025-11-07 11:51:25 -0300

    FEAT: criando a barra animada de baixo dos itens #2

--- src/components/HeaderComponents.vue
+++ src/components/HeaderComponents.vue
@@ -14,12 +14,22 @@ const pages = [
 <template>
         <div class="bg-black/80 text-white m-10 rounded-4xl shadow-xl">
             <nav class="flex justify-between">
-                <h1 class="mt-5 mb-5 ml-10"><RouterLink to="/">Ghibli</RouterLink></h1>
+                <h1 class="mt-5 mb-5 ml-10"><RouterLink to="/"></RouterLink></h1>
                 <ul class="flex mr-30 mb-5 mt-5">
-                    <li v-for="item of pages" :key="item" class="ml-17 hover:opacity-50 transition-all duration-500 bg-zinc-800 rounded-2xl pr-4 pl-4 pt-1 pb-1">
-                        <RouterLink :to="item.link">{{ item.name }}</RouterLink>
+                    <li v-for="item of pages" :key="item" class="ml-17 transition-all duration-500 rounded-2xl pr-4 pl-4 pt-1 pb-1 group">
+                        <RouterLink class="link" :to="item.link">{{ item.name }}</RouterLink>
+                        <div
+                            class="h-[2px] bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"
+                        ></div>
                     </li>
                 </ul>
             </nav>
         </div>
 </template>
+
+<style scoped>
+    .bar:hover {
+        animation: show-bar 1s ease-in;
+    }
+    
+</style>
