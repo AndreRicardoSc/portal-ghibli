f3c42e4c27d338a78bf7a334d0553699e59ded9a
AndreRicardoSc <andrenarutto7@gmail.com>
2025-11-11 11:36:19 -0300

    FEAT: Iniciadno o desenvolvimento do banner #4

---
+++ src/components/BannerComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+    const props = defineProps(['title', 'subtitle', 'buttonText'])
+</script>
+<template>
+    <section 
+        class="flex flex-col justify-center items-center gap-10 w-screen h-screen bg-center bg-no-repeat bg-cover bg-[url('/images/banner/background-banner.png')]"
+    >
+        <div class="absolute inset-0 bg-gradient-to-b from-transparent to-white/60"></div>
+        <div class="text-center flex flex-col gap-2">
+            <h1 class="text-5xl">{{ props.title }}</h1>
+            <p class="text-xl text-black/70">{{ props.subtitle }}</p>
+        </div>
+        <button class="text-2xl bg-pink-500 py-2 px-4 rounded-2xl max-w-70">{{ props.buttonText }}</button>
+    </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,12 @@
 
     //     peopleDetails.value = details;
     // })
+    import BannerComponent from '../components/BannerComponent.vue';
 </script>
 <template>
-    
+    <BannerComponent
+        :title="'Descubra o Universo Ghibli'"
+        :subtitle="'Conheça as obras do estúdio que marcou gerações de pessoas de todas as idades'"
+        :button-text="'Vamos conhecer os filmes!'"
+    />
 </template>
