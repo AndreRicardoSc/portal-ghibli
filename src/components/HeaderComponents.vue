eb729366e2a4677c164d89033f5f0234cac07f9d
Davidospassos <davidospassos.848@gmail.com>
2025-11-11 11:10:34 -0300

    Feat: botando a logo no site #2

--- src/components/HeaderComponents.vue
+++ src/components/HeaderComponents.vue
@@ -14,9 +14,11 @@ const pages = [
 <template>
         <div class="bg-black/80 text-white m-10 rounded-4xl shadow-xl">
             <nav class="flex justify-between">
-                <h1 class="mt-5 mb-5 ml-10"><RouterLink to="/"></RouterLink></h1>
+                <RouterLink to="/">
+                    <img src="/images/logo-ghibli.png" alt="logo" class="w-15 h-20 ml-5 mt-0.5"></img>
+                </RouterLink>
                 <ul class="flex mr-30 mb-5 mt-5">
-                    <li v-for="item of pages" :key="item" class="ml-17 transition-all duration-500 rounded-2xl pr-4 pl-4 pt-1 pb-1 group">
+                    <li v-for="item of pages" :key="item" class="ml-17 transition-all duration-500 rounded-2xl pr-4 pl-4 pt-2 pb-2 group">
                         <RouterLink class="link" :to="item.link">{{ item.name }}</RouterLink>
                         <div
                             class="h-[2px] bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"
@@ -26,7 +28,6 @@ const pages = [
             </nav>
         </div>
 </template>
-
 <style scoped>
     .bar:hover {
         animation: show-bar 1s ease-in;
