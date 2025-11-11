fb28a8cbff4dde02a929aa5ce715264765930d71
Davidospassos <davidospassos.848@gmail.com>
2025-11-11 15:13:38 -0300

    Feat: Comecando o footer de site #5

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
   import HeaderComponents from './components/HeaderComponents.vue';
+  import FooterComponents from './components/FooterComponents.vue';
 </script>
 <template>
   <header>
     <HeaderComponents/>
   </header>
   <RouterView/>
+  <footer>
+    <FooterComponents/>
+  </footer>
 </template>
 
