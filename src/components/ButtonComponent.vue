1c0c1fff7e09ab7b8ec578b21adb91eee896be1c
AndreRicardoSc <andrenarutto7@gmail.com>
2025-11-12 13:27:25 -0300

    FEAT: Consolidando o Banner e animando #4

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { motion } from 'motion-v';
+    const props = defineProps({
+        buttonText: String,
+        color: String,
+        link: String,
+        animate: Boolean
+    }); 
+</script>
+<template>
+    <RouterLink
+        :class="`text-2xl text-center py-2 px-6 rounded-3xl max-w-100 border-2 border-transparent bg-${props.color} hover:bg-${props.color}/30 hover:border-${props.color} hover:text-${props.color} transition-all duration-500 md:text-xl`" 
+        :to="props.link"
+    >
+        {{ props.buttonText }}
+    </RouterLink>
+</template>
