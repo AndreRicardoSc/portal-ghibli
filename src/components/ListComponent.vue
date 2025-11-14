cd10bfef30e34d54719c1cb74b1a02fcf07c3545
AndreRicardoSc <andrenarutto7@gmail.com>
2025-11-14 12:02:34 -0300

    FEAT: Criando a base dos cards #8

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import MovieCard from './MovieCard.vue';
+    import getImageUrl from '../services/images';
+    const props = defineProps(['movieList'])
+</script>
+<template>
+    <ul class="flex flex-col items-start">
+        <li v-for="movie in props.movieList">
+            <MovieCard 
+                :image="getImageUrl(movie?.poster_path)"
+                :name="movie?.title"
+                :sinopse="movie?.overview"
+            />
+        </li>
+    </ul>
+</template>
---
+++ src/components/MovieCard.vue
@@ -0,0 +1,12 @@
+<script setup>
+    const props = defineProps(['image', 'name', 'sinopse', 'classification']);
+</script>
+<template>
+    <div>
+        <img :src="props.image" :alt="`${props.name}-image`">
+        <div>
+            <h3>{{ props.name }}</h3>
+            <p>{{ props.sinopse }}</p>
+        </div>
+    </div>
+</template>
