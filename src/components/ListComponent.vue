d1365a373de00c0cae57d60c3ebc6e24ea658c1d
AndreRicardoSc <andrenarutto7@gmail.com>
2025-11-17 07:53:12 -0300

    FEAT: Base dos cards responsíveis criadas #8

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -1,15 +1,24 @@
 <script setup>
+    import { onMounted } from 'vue';
     import MovieCard from './MovieCard.vue';
     import getImageUrl from '../services/images';
-    const props = defineProps(['movieList'])
+    import { useGenreStore } from '../stores/genre';
+    const genreStore = useGenreStore();
+
+    const props = defineProps(['movieList']);
+
+    onMounted(() => {
+        genreStore.getGenres();
+    })
 </script>
 <template>
-    <ul class="flex flex-col items-start">
+    <ul class="flex flex-col items-center justify-center gap-20 md:flex-row md:flex-wrap md:gap-10">
         <li v-for="movie in props.movieList">
             <MovieCard 
                 :image="getImageUrl(movie?.poster_path)"
                 :name="movie?.title"
-                :sinopse="movie?.overview"
+                :genres="movie?.genre_ids"
+                :classification="movie.vote_average"
             />
         </li>
     </ul>
--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,12 +1,33 @@
 <script setup>
-    const props = defineProps(['image', 'name', 'sinopse', 'classification']);
+    import { motion } from 'motion-v';
+
+    import { useGenreStore } from '../stores/genre';
+    const genreStore = useGenreStore();
+
+    const props = defineProps(['image', 'name', 'classification', 'genres']);
+
 </script>
 <template>
-    <div>
-        <img :src="props.image" :alt="`${props.name}-image`">
-        <div>
-            <h3>{{ props.name }}</h3>
-            <p>{{ props.sinopse }}</p>
+    <motion.div class="flex flex-col w-70 items-center cursor-pointer border py-5 px-10 rounded-3xl md:max-h-96 md:min-h-96"
+        :hover="{ scale: 1.05 }"
+    >
+        <img class="w-100 rounded-4xl md:w-40" 
+            :src="props.image" 
+            :alt="`${props.name}-image`"
+        >
+        <div class="flex flex-col items-center gap-5 my-4 md:items-start">
+            <div class="flex gap-2 items-center">
+                <h3 class="text-2xl line-clamp-2 font-semibold text-start md:text-lg md:line-clamp-3">{{ props.name }}</h3>
+                <p class="flex gap-1 text-lg text-amber-500 md:text-sm">
+                    <span class="mdi mdi-star"></span>
+                    {{ props?.classification.toFixed(1).replace('.', ',') }}
+                </p>
+            </div>
+            <ul class="flex flex-wrap gap-4 md:hidden">
+                <li class="border rounded-2xl py-1 px-2 md:text-xs" v-for="genreId in props.genres">
+                    {{ genreStore.getGenreName(genreId) }} 
+                </li>
+            </ul>
         </div>
-    </div>
+    </motion.div>
 </template>
