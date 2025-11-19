8cba9aab81eb5d085d245dec5fcec82ddc99f067
AndreRicardoSc <andrenarutto7@gmail.com>
2025-11-18 22:11:48 -0300

    FEAT: Iniciando a criação da paginação #10

---
+++ src/components/PagesComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import { useMovieStore } from '../stores/movie';
+    const movieStore = useMovieStore();
+
+
+</script>
+<template>
+    <ul>
+        <li v-for="page in movieStore.pages">
+            {{ page }}
+        </li>
+    </ul>
+</template>
--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -2,6 +2,8 @@
     import { onMounted } from 'vue';
     import ListComponent from '../components/ListComponent.vue';
     import TopPageComponent from '../components/TopPageComponent.vue';
+    import PagesComponent from '../components/PagesComponent.vue';
+    
     import { useGenreStore } from '../stores/genre';
     import { useMovieStore } from '../stores/movie';
 
@@ -29,4 +31,5 @@
     <ListComponent
         :movie-list="movieStore.movies"
     />
+    <PagesComponent/>
 </template>
