ce8912684ea112d4529b8b404693f626b0192179
AndreRicardoSc <andrenarutto7@gmail.com>
2025-11-25 10:44:14 -0300

    FEAT: Ajustando a TopPageComponent #12

--- src/components/TopPageComponent.vue
+++ src/components/TopPageComponent.vue
@@ -9,7 +9,13 @@
 </script>
 <template>
     <section class="flex items-center justify-center gap-20 pt-40 md:pt-30 pb-20 shadow-xl px-10 md:px-30">
-        <div class="flex flex-col gap-5 md:gap-2">
+        <div class="flex flex-col gap-5 md:gap-2"
+            :class="
+                props.list
+                ? 'text-justify'
+                : 'text-center items-center md:px-40 md:gap-3'
+            "
+        >
             <motion.h1 class="flex items-center gap-5 md:items-end text-primary text-3xl md:text-4xl font-semibold">
                 <p>{{ props.title }}</p>
                 <span v-if="props.icon" :class="props.icon"></span>
@@ -18,6 +24,6 @@
                 {{ props.subtitle }}
             </motion.p>
         </div>
-        <list/>
+        <list v-if="props.list"/>
     </section>
 </template>
