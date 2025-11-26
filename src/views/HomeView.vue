5019baedf4eaea71120d3c06350b5750fcf2dc42
Davidospassos <davidospassos.848@gmail.com>
2025-11-26 18:58:02 -0300

    FEAT: Continuando a homepage fazendo os personagens iconicos #14

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,23 +24,23 @@
         :subtitle="banner.subtitle"
         :button-text="banner.buttonText"
     />
-    <div class="bg-blue-100">
-        <h1 class="text-center text-2xl mb-16">Personagens Icônicos</h1>
+    <div>
+        <h1 class=" text-secondary ml-10 font-bold text-4xl mb-16">Personagens Icônicos</h1>
         <ul class="flex justify-center">
             <li class="flex flex-col items-center">
-                <img src="/images/banner/CharacterPonyo.jpg" alt="" class="rounded-2xl">
-                <p class="text-2xl">Ponyo</p>
-                <p>Ela é uma pequena criatura mágica semelhante a um peixe-dourado, filha de uma deusa do mar e de um feiticeiro humano</p>
+                <p class="bg-blue-300 p-1.5 rounded-2xl"><img src="/images/banner/CharacterPonyo.jpg" alt="Ponyo" class="rounded-2xl"></p>
+                <p class="text-2xl mb-2 font-bold text-secondary">Ponyo</p>
+                <p>Ela é uma pequena criatura mágica semelhante a um peixe-dourado, filha de uma deusa do mar e de um feiticeiro humano.</p>
             </li>
             <li class="flex flex-col items-center">
-                <img src="/images/banner/TOTORO.jpg" alt="" class="rounded-2xl">
-                <p class="text-2xl">Totoro</p>
-                <p>Totoro é uma criatura mágica e gentil que vive na floresta. Grande, peludo e com aparência fofa, ele simboliza a natureza, a imaginação e o conforto infantil</p>
+                <p class="bg-blue-300 p-1.5 rounded-2xl"><img src="/images/banner/TOTORO.jpg" alt="Totoro" class="rounded-2xl"></p>
+                <p class="text-2xl mb-2 font-bold text-secondary">Totoro</p>
+                <p>Totoro é uma criatura mágica e gentil que vive na floresta. Grande, peludo e com aparência fofa, ele simboliza a natureza, a imaginação e o conforto infantil.</p>
             </li>
             <li class="flex flex-col items-center">
-                <img src="/images/banner/haku.jpg" alt="" class="rounded-2xl">
-                <p class="text-2xl">Haku</p>
-                <p>Haku é um garoto misterioso do filme A Viagem de Chihiro, que também possui a forma de um dragão branco. Inteligente, leal e enigmático.</p>
+                <p class="bg-blue-300 p-1.5 rounded-2xl"><img src="/images/banner/haku.jpg" alt="Haku" class="rounded-2xl"></p>
+                <p class="text-2xl mb-2 font-bold text-secondary">Haku</p>
+                <p>Haku é um garoto misterioso, que também possui a forma de um dragão branco. Inteligente, leal e enigmático.</p>
             </li>
         </ul>
     </div>
@@ -51,8 +51,8 @@
         height: 160px;
     }
     ul li{
-        background-color: lightskyblue;
-        margin: 10px 10px 10px 10px;
+        background-color: rgb(192, 221, 240);
+        margin: 10px 30px 10px 30px;
         border-radius: 30px;
         width: 400px;
         padding-top: 20px;
